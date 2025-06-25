9abfc7580aca44123fdb7bbe56259adf0b87d1b5
TB3104 <tb310438@student.polsl.pl>
2025-06-25 20:44:57 +0200

    AppConfiguration i KeyVault

--- CompliantManager.Server/Program.cs
+++ CompliantManager.Server/Program.cs
@@ -1,5 +1,20 @@
-using CompliantManager.Server.Data;
+﻿using CompliantManager.Server.Data;
+using CompliantManager.Server.Data.Entities;
+using CompliantManager.Server.Services.Implementations;
+using CompliantManager.Server.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+<<<<<<< HEAD
+using Azure.Identity;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+=======
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+>>>>>>> d8d80e21dfcf0e2ed874bdc116b0957b0306702a
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,12 +27,61 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Server=.;Database=CompliantManagerDB;Trusted_Connection=True;Encrypt=False;"));
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        RoleClaimType = ClaimTypes.Role
+    };
+});
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<InitialSetupService>();
+
+builder.Services.AddAuthorization();
+
+//Oznaczam �e to ja (TYmon) tutaj psuje
+builder.Configuration.AddAzureAppConfiguration(options =>
+{
+    var connStr = builder.Configuration.GetConnectionString("AppConfig");
+    options.Connect(connStr)
+           .ConfigureKeyVault(kv =>
+           {
+               kv.SetCredential(new DefaultAzureCredential());
+           });
+});
+//Koniec psucia
 
 var app = builder.Build();
 
 using var scope = app.Services.CreateScope();
+
 var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 dbContext.Database.Migrate();
+
+var setupService = scope.ServiceProvider.GetRequiredService<InitialSetupService>();
+if (!setupService.IsInitialized())
+{
+    var email = "admin@example.com";
+    var password = "Admin123!";
+    var firstName = "Admin";
+    var lastName = "User";
+
+    await setupService.InitializeAsync(email, password, firstName, lastName);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -32,6 +96,7 @@ app.UseHttpsRedirection();
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
